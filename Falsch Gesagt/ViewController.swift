e678e63ae6a06b252ef1f837e0e12026f7c9851f
Josef Büttgen <josefbuettgen@gmail.com>
2018-06-18 14:25:41 +0200

    Initial

---
+++ Falsch Gesagt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Falsch Gesagt
+//
+//  Created by Josef Büttgen on 18.06.18.
+//  Copyright © 2018 Dsrptrs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Falsch GesagtTests/Falsch_GesagtTests.swift
@@ -0,0 +1,36 @@
+//
+//  Falsch_GesagtTests.swift
+//  Falsch GesagtTests
+//
+//  Created by Josef Büttgen on 18.06.18.
+//  Copyright © 2018 Dsrptrs. All rights reserved.
+//
+
+import XCTest
+@testable import Falsch_Gesagt
+
+class Falsch_GesagtTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
